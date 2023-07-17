813c3246b8b0a3afe1bd322c7e619644a46d23e5
Mert Çetin <mrtctn506@gmail.com>
2023-07-18 01:04:03 +0300

    step1 and step2 bug fixed

--- dist/app.js
+++ dist/app.js
@@ -3,23 +3,44 @@ const main1DOM = document.querySelector('.main-1');
 const main2DOM = document.querySelector('.main-2');
 const circle1DOM = document.querySelector('.circle1');
 const circle2DOM = document.querySelector('.circle2');
-const planSelectDOM = document.querySelector('.planSelect')
+
+nextbtnDOM.addEventListener('click', () => {
+    main2DOM.style.display = 'block';
+    main1DOM.style.display = 'none';
+
+    circle2DOM.classList.toggle('active');
+    circle1DOM.classList.add('active');
+});
+
+
+
+const planSelectDOM = document.querySelector('.planSelect');
 const planCircleDOM = document.querySelector('.planCircle');
 const monthlyDOM = document.querySelector('.monthly');
 const yearlyDOM = document.querySelector('.yearly');
-
-nextbtnDOM.addEventListener('click', () => {
-    main2DOM.style.display = 'block'
-    main1DOM.remove();
-
-    circle1DOM.classList.add('active');
-    circle2DOM.classList.add('active');
-});
 
 planCircleDOM.addEventListener('click', () => {
     planCircleDOM.classList.toggle('moved');
 
     monthlyDOM.classList.toggle('active-monthly');
-    yearlyDOM.classList.toggle('active-yearly');
+    yearlyDOM.classList.remove('active-yearly');
 });
 
+
+const goback1DOM = document.querySelector('.go-back1');
+
+goback1DOM.addEventListener('click', () => {
+    main1DOM.style.display = 'block';
+    main2DOM.style.display = 'none';
+
+    circle2DOM.classList.toggle('active');
+    circle1DOM.classList.remove('active');
+});
+
+
+
+// const nextbtn1DOM = document.querySelector('.next-btn1');
+
+// nextbtn1DOM.addEventListener('click', () => {
+    
+// })
