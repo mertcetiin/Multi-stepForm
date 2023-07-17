f81cc143b8d1be786e2bc2b6eb164e41570ee3f4
Mert Çetin <mrtctn506@gmail.com>
2023-07-17 17:51:24 +0300

    switcher made

--- dist/app.js
+++ dist/app.js
@@ -3,8 +3,10 @@ const main1DOM = document.querySelector('.main-1');
 const main2DOM = document.querySelector('.main-2');
 const circle1DOM = document.querySelector('.circle1');
 const circle2DOM = document.querySelector('.circle2');
-const step2DOM = document.querySelector('.step2')
-
+const planSelectDOM = document.querySelector('.planSelect')
+const planCircleDOM = document.querySelector('.planCircle');
+const monthlyDOM = document.querySelector('.monthly');
+const yearlyDOM = document.querySelector('.yearly');
 
 nextbtnDOM.addEventListener('click', () => {
     main2DOM.style.display = 'block'
@@ -13,3 +15,11 @@ nextbtnDOM.addEventListener('click', () => {
     circle1DOM.classList.add('active');
     circle2DOM.classList.add('active');
 });
+
+planCircleDOM.addEventListener('click', () => {
+    planCircleDOM.classList.toggle('moved');
+
+    monthlyDOM.classList.toggle('active-monthly');
+    yearlyDOM.classList.toggle('active-yearly');
+});
+
